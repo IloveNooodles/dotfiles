d1c8a745c5d60ac5831e8afdcdec26c47dd437cc
IloveNooodles <mgarebaldhie80@gmail.com>
2024-11-21 15:48:16 +0900

    feat: update the configurations for fish and vimux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ source ~/.config/fish/conf.d/abbr.fish
 source ~/.config/fish/conf.d/alias.fish
 source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/path.fish
+source ~/.config/fish/conf.d/functions.fish
 
 ## Plugins
 starship init fish | source
