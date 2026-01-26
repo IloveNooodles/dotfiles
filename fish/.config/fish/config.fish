7e00b261d3c06d56f594440205c093b2adb888c1
IloveNooodles <mgarebaldhie80@gmail.com>
2026-01-27 01:15:52 +0900

    update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,3 +17,10 @@ eval (tmuxifier init - fish)
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# pnpm
+set -gx PNPM_HOME "/Users/rahman.muhammad/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
