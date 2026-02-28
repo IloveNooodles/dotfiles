e8bb197f24961e9e4f6384b91f85be23b6a27dae
IloveNooodles <mgarebaldhie80@gmail.com>
2026-02-28 15:07:57 +0900

    nix: update system

--- nix/.config/nix/modules/system.nix
+++ nix/.config/nix/modules/system.nix
@@ -4,7 +4,7 @@
   nix.settings = {
     experimental-features = "nix-command flakes";
     substituters = [ 
-      "https://cache.nixos.org", 
+      "https://cache.nixos.org" 
       "https://nix-community.cachix.org" 
     ];
     trusted-public-keys = [ 
